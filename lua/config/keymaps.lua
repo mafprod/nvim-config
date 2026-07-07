0ef6358d414bf487bda19af32b55506ce0434227
MafProd <109171006+mafprod@users.noreply.github.com>
2026-07-07 22:02:13 +0200

    Added keymaps for CodeCompanion

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,16 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+vim.keymap.set(
+  "n",
+  "<LocalLeader>a",
+  "<cmd>CodeCompanionChat<CR>",
+  { noremap = true, silent = true }
+)
+
+vim.keymap.set(
+  "n",
+  "<LocalLeader>aa",
+  "<cmd>CodeCompanionActions<CR>",
+  { noremap = true, silent = true }
+)
