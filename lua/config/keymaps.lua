55480287232e9a19a920755c7b787303723343b8
MafProd <109171006+mafprod@users.noreply.github.com>
2026-07-08 12:31:24 +0200

    Updated keymaps
    Added descriptions and keymaps to quickly set the spell language for the
    buffer.

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,6 +1,8 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- Open CodeCompanion chat window
 vim.keymap.set(
   "n",
   "<LocalLeader>a",
@@ -8,9 +10,26 @@ vim.keymap.set(
   { noremap = true, silent = true }
 )
 
+-- Open the CodeCompanion Actions popup
 vim.keymap.set(
   "n",
   "<LocalLeader>aa",
   "<cmd>CodeCompanionActions<CR>",
+  { noremap = true, silent = true }
+)
+
+-- Set local spell language to Dutch
+vim.keymap.set(
+  "n",
+  "<LocalLeader>sn",
+  "<cmd>setlocal spelllang=nl<CR>",
+  { noremap = true, silent = true }
+)
+
+-- Set local spell language to English
+vim.keymap.set(
+  "n",
+  "<LocalLeader>se",
+  "<cmd>setlocal spelllang=en<CR>",
   { noremap = true, silent = true }
 )
